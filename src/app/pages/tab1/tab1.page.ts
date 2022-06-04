a889e9917534806daf9d7ed905fcf644001519e3
Rodrigo Mtz <88547726+theRobert174@users.noreply.github.com>
2022-06-03 20:26:25 -0500

    Tarea inifinte scroll

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Article } from 'src/app/interfaces';
 import { NewsService } from '../../services/news.service';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,10 +11,28 @@ import { NewsService } from '../../services/news.service';
 export class Tab1Page implements OnInit{
 
   public articles : Article[] = [];
+  @ViewChild(IonInfiniteScroll, { static:true }) infiniteScroll: IonInfiniteScroll;
 
-  constructor(private newService : NewsService) {}
+  constructor(private newsService : NewsService) {}
 
   ngOnInit() {
-    this.newService.getTopHeadLines().subscribe( articles => this.articles.push( ...articles )); //... desestructuracion
+    this.newsService.getTopHeadLines().subscribe( articles => this.articles.push( ...articles )); //... desestructuracion
+  }
+
+  loadData(){
+    this.newsService.getTopheadLinesByCategory( 'business', true).subscribe(articles => {
+
+      //Opcion para controlar el infinite scroll pero no es muy recomendata por dar un error falso
+      //30 articulos de sport puede ser igual a 30 articulos en health
+      if(articles.length === this.articles.length){
+        this.infiniteScroll.disabled = true;
+        //event.target.disabled = true;
+        return;
+      }
+
+      this.articles = articles;
+      this.infiniteScroll.complete();
+      //event.target.complete();
+    })
   }
 }
